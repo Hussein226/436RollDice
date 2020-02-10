7e8783c6bc07f82423bb3474c3e14ad8aaae8413
Hussein Hourani <houranihussein@gmail.com>
2020-02-10 17:35:07 -0500

    Fixed turn total

--- app/src/main/java/com/example/a436rolldice/RollDiceActivity.java
+++ app/src/main/java/com/example/a436rolldice/RollDiceActivity.java
@@ -10,6 +10,9 @@ import android.widget.ImageView;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
@@ -17,12 +20,21 @@ public class RollDiceActivity extends AppCompatActivity {
 
     ImageView Dice_1, Dice_4; //I messed up and it become dice 4
 
+    TextView Player1_Score, Player2_Score, Current_Player, Turn_Total;
+
+    int player1Score,player2Score,turnTotal;
+    String currentPlayer;
+
+    int diceTotal, totalScore;
+
     ImageView[] diceArray = {};
     private Random rng = new Random();
 
 
 
-    private Button RollDice;
+
+
+    private Button RollDice, holdButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +52,37 @@ public class RollDiceActivity extends AppCompatActivity {
 //        diceArray[5] = Dice_6;
         // We dont need all of these but maybe store Dice 1 and Dice 2 in an array
 
-
+        //Button Initializing
         RollDice = (Button) findViewById(R.id.RollDice);
+        holdButton = (Button) findViewById(R.id.hold);
+
+        //TextView Initializing
+        Player1_Score = (TextView) findViewById(R.id.player1);
+        Player2_Score = (TextView) findViewById(R.id.player2);
+        Current_Player = (TextView) findViewById(R.id.currPlayer);
+        Turn_Total = (TextView) findViewById(R.id.turnTotal);
 
         RollDice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 changeDiceFace();// We dont need 2 functions for both faces, just one function that will change the faces of both dice
-                changeDiceFace2();
+
+                if(turnTotal < 50){
+                    Turn_Total.setText("Turn total: " + turnTotal);
+                }
+                else if (turnTotal >= 50){
+
+                }
+
+            }
+        });
+
+        holdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Hold_Button();
             }
         });
 
@@ -56,62 +91,90 @@ public class RollDiceActivity extends AppCompatActivity {
     private void changeDiceFace(){
 
         int randomNumber = rng.nextInt(6) + 1;
+        int randomNumber2 = rng.nextInt(6) + 1;
+
+        boolean isOne = false, isOne2 = false;
 
 
 
         switch(randomNumber){
             case 1:
                 Dice_1.setImageResource(R.drawable.dice_1);
+                isOne = true;
+                turnTotal += 1;
                 break;
             case 2:
                 Dice_1.setImageResource(R.drawable.dice_2);
+                turnTotal += 2;
                 break;
             case 3:
                 Dice_1.setImageResource(R.drawable.dice_3);
+                turnTotal += 3;
                 break;
             case 4:
                 Dice_1.setImageResource(R.drawable.dice_4);
+                turnTotal += 4;
                 break;
             case 5:
                 Dice_1.setImageResource(R.drawable.dice_5);
+                turnTotal += 5;
                 break;
             case 6:
                 Dice_1.setImageResource(R.drawable.dice_6);
+                turnTotal += 6;
                 break;
 
 
-        }
-
-    }
-
-    private void changeDiceFace2(){
-
-        int randomNumber = rng.nextInt(6) + 1;
-
-
-
-        switch(randomNumber){
+            }
+        switch(randomNumber2){
             case 1:
                 Dice_4.setImageResource(R.drawable.dice_1);
+                isOne2 = true;
+                turnTotal += 1;
                 break;
             case 2:
                 Dice_4.setImageResource(R.drawable.dice_2);
+                turnTotal += 2;
                 break;
             case 3:
                 Dice_4.setImageResource(R.drawable.dice_3);
+                turnTotal += 3;
                 break;
             case 4:
                 Dice_4.setImageResource(R.drawable.dice_4);
+                turnTotal += 4;
                 break;
             case 5:
                 Dice_4.setImageResource(R.drawable.dice_5);
+                turnTotal += 5;
                 break;
             case 6:
                 Dice_4.setImageResource(R.drawable.dice_6);
+                turnTotal += 6;
                 break;
 
 
         }
+
+        if(isOne && isOne2){
+
+            turnTotal = 0;
+            Turn_Total.setText("Turn total: " + turnTotal);
+
+        }
+
+
+    }
+
+
+
+    private void Hold_Button(){
+
+        turnTotal = 0;
+
+        Turn_Total.setText("Turn total: 0");
+
+
 
     }
 }
